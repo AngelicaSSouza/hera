4a181c4b71cd9d0278ab462e92a36c956120e3fd
Uillian Lopes <uillian.lopes@worthix.com>
2024-04-05 20:55:04 -0300

    adiciona arquivos html

--- PycharmProjects/hera/core/views.py
+++ PycharmProjects/hera/core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def cadastro(request):
+    return render(request,'cadastro.html')
+
+def catalago(request):
+    return render(request, 'catalago.html')
+
+def contato(request):
+    return render(request, 'contato.html')
+
